47abed7160b372e6c9ccfb1062206786c76eba3b
Petr Kubica <cubicap@email.cz>
2026-04-04 00:47:01 +0200

    add support for esp32-s3 with octal spi psram

--- main/platform/esp32s3.h
+++ main/platform/esp32s3.h
@@ -6,6 +6,8 @@
 
 #include "espCommon.h"
 
+#include <sdkconfig.h>
+
 
 struct PlatformInfo {
     static inline constexpr std::string NAME = "ESP32-S3";
@@ -13,7 +15,10 @@ struct PlatformInfo {
     struct PinConfig {
         static inline const std::set<int> DIGITAL_PINS = {
             0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 21,
-            33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 45, 46, 47, 48
+#ifndef CONFIG_SPIRAM_MODE_OCT
+            33, 34, 35, 36, 37,
+#endif
+            38, 39, 40, 41, 42, 45, 46, 47, 48
         };
         static inline const std::unordered_map<int, std::pair<int, int>> ANALOG_PINS = {
             { 1, { 1, ADC1_GPIO1_CHANNEL } },
